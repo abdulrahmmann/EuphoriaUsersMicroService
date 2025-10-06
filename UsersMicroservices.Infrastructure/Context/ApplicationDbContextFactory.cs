c9c3f6da5ca73120f2bc89db4be1841628737939
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-07 02:07:31 +0300

    Create DbContext and DbContextFactory.

---
+++ UsersMicroservices.Infrastructure/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace UsersMicroservices.Infrastructure.Context;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        const string connectionString = "Host=localhost;Port=5432;Database=euphoriaUsers;Username=postgres;Password=123";
+
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}

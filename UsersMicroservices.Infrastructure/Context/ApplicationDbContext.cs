25fe4a7b183e36024d019a365ee20ce82542f436
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-07 02:07:31 +0300

    Create DbContext and DbContextFactory.

---
+++ UsersMicroservices.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersMicroservices.Domain.Entities;
+
+namespace UsersMicroservices.Infrastructure.Context;
+
+public class ApplicationDbContext: DbContext
+{
+    public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    public DbSet<UserProfile> UserProfile { get; set; }
+    
+    protected ApplicationDbContext() { }
+    public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}

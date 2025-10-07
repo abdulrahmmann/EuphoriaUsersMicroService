25ff74a321be93ec97383ecb5e8e66c124db5550
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-07 15:00:01 +0300

    Create Initial Migration.

---
+++ UsersMicroservices.Infrastructure/Migrations/20251007115853_Initial.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UsersMicroservices.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Username = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    Gender = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp(6) without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    CreatedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "timestamp(6) without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    LastModifiedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsersProfile",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FirstName = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    SecondName = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: true),
+                    LastName = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    FullName = table.Column<string>(type: "text", nullable: false),
+                    ProfileImage = table.Column<string>(type: "text", nullable: true),
+                    Biography = table.Column<string>(type: "character varying(600)", maxLength: 600, nullable: false),
+                    Country = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    City = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    Street = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Address_ZipCode = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp(6) without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    CreatedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "timestamp(6) without time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    LastModifiedBy = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersProfile", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UsersProfile_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Gender",
+                table: "Users",
+                column: "Gender",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PhoneNumber",
+                table: "Users",
+                column: "PhoneNumber",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersProfile_FullName",
+                table: "UsersProfile",
+                column: "FullName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersProfile_UserId",
+                table: "UsersProfile",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersProfile");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}

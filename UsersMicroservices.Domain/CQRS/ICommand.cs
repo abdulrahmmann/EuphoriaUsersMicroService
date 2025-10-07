59257257263d7f90345b1bb2b7557bfaf57429b5
Abdulrahman <abd3lra7man.078@gmail.com>
2025-10-07 15:38:00 +0300

    Create CQRS Bases.

---
+++ UsersMicroservices.Domain/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace UsersMicroservices.Domain.CQRS;
+
+public interface ICommand<out TResponse>: IRequest<TResponse>
+{
+    
+}
+
+public interface ICommand : ICommand<Unit>
+{
+    
+}
